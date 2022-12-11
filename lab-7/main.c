75dcacf1d355afce0e83588d5258bb6c4c7036f1
Pavel-Shcherbo <44784481+Pavel-Shcherbo@users.noreply.github.com>
2022-12-11 16:22:33 +0300

    1

--- lab-7/main.c
+++ lab-7/main.c
@@ -5,9 +5,8 @@
 int main() {
     printf("What task you want to solve 1 or 2?");
     void(*function[2])(void) = { task1, task2 };
-    int num = 0, choice = 0;
+    unsigned int num = 0;
     scanf_s("%d", &num);
-    choice = num - 1;
-    function[choice - 1]();
+    function[num - 1]();
 }
 
